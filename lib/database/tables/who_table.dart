508e61866dc58f015a40624f32526c97b5097852
evgenymay <evgenymay@gmail.com>
2026-04-07 01:17:52 +0300

    2

--- lib/database/tables/who_table.dart
+++ lib/database/tables/who_table.dart
@@ -4,6 +4,6 @@ class ItemTable extends Table {
   IntColumn get id => integer().autoIncrement()();
   IntColumn get phone => integer()();
   TextColumn get name => text().withLength(min: 1)();
-  TextColumn get description => text().nullable()();
+  TextColumn get tags => text().nullable()();
   DateTimeColumn get registered => dateTime().nullable()();
 }
